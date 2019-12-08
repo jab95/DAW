c05166e2a1dcac3e67a442a2194fa9de321459d9
jab95 <josejab95@gmail.com>
2019-12-08 17:30:12 +0100

    añade postal diw

---
+++ DIW/postal-navideña/script.js
@@ -0,0 +1,12 @@
+
+
+$.fn.christmas = function () {
+    $(this).each(function () {
+        $(this).html($(this).text().split("").map(function (v, i) {
+            return '<span class="christmas-' + (i % 2 == 0 ? 'gold' : 'red') + '">' + v + '</span>';
+        }).join(""));
+    });
+};
+$(function () { // don't forget $(document).ready!
+    $('h1.christmas').christmas();
+});
